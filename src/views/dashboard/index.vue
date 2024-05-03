d7d74c74746279cb6ae041a838827dca3d1209d0
auooru <auooru@outlook.com>
2024-05-03 23:16:00 +0800

    chore: 调整主页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup>
 import { useUserStore } from "@/store/modules/user"
-import Admin from "./components/Admin.vue"
-import Editor from "./components/Editor.vue"
 
 const userStore = useUserStore()
-const isAdmin = userStore.roles.includes("admin")
 </script>
 
 <template>
-  <component :is="isAdmin ? Admin : Editor" />
+  <div class="p-4">
+    <h1>Welcome {{ userStore.username }} !</h1>
+  </div>
 </template>
